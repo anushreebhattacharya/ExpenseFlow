489083a601127aa11039a0dcf598480cbcdf4cc2
Anushree Bhattacharya <anushreebhattacharya606@gmail.com>
2026-06-11 15:50:30 +0530

    Update expenseServices.js

--- Frontend/src/services/expenseServices.js
+++ Frontend/src/services/expenseServices.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL="https://expenseflow-backend-d0fm.onrender.com/";
+const API_URL="https://expenseflow-backend-d0fm.onrender.com/api/";
 
 const getToken=()=>{
   const user=JSON.parse(localStorage.getItem("user"));
