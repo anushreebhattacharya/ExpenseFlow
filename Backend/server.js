effcaa04faa5e31faef11951567a22193a4c2fd4
anushreebhattacharya <anushreebhattacharya606@gmail.com>
2026-06-11 15:39:13 +0530

    updated CORS Origin

--- Backend/server.js
+++ Backend/server.js
@@ -9,8 +9,8 @@ connectDB();
 
 const app=express();
 app.use(cors({
-    origin:'http://localhost:5173',
-    credentials:true
+    origin: '*', 
+    credentials: true
 }));
 app.use(express.json());
 
